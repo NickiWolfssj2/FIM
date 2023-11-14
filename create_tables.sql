8fa8887521652b417dac28ee1f8e92db70b05a82
Brayan <bviaorellana@cifpfbmoll.eu>
2023-11-14 21:13:10 +0100

    tabla piloto

---
+++ create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE piloto (
+    id INT PRIMARY KEY,
+    nom VARCHAR(50),
+    llinatge VARCHAR(50),
+    alies VARCHAR(50),
+    num_dorsal INT,
+    pais VARCHAR(50),
+    equip VARCHAR(50)
+);
