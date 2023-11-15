f79530518a72d0418f2c4bb8817675fe34e864d3
Brayan <bviaorellana@cifpfbmoll.eu>
2023-11-15 20:00:07 +0100

    Creacion de varias tablas, hasta FOREIGN KEY

--- create_tables.sql
+++ create_tables.sql
@@ -8,9 +8,48 @@ CREATE TABLE piloto (
     equip VARCHAR(50)
 );
 
-CREATE TABLE equipo (
+CREATE TABLE equip (
     id INT PRIMARY KEY,
     nom_equip VARCHAR(50),
     cap VARCHAR(50),
-    marca VARCHAR(50)
+    marca VARCHAR(30)
+);
+
+CREATE TABLE circuit (
+    id INT PRIMARY KEY,
+    nom VARCHAR (50),
+    longitud INT,
+    telefon INT,
+    record VARCHAR(50),
+    pais VARCHAR(50)
+);
+
+CREATE TABLE gp (
+    id INT PRIMARY KEY
+);
+
+CREATE TABLE resultat (
+    id INT PRIMARY KEY
+);
+
+CREATE TABLE persona (
+    id INT PRIMARY KEY  
+);
+
+CREATE TABLE categoria (
+    id INT PRIMARY KEY
+);
+
+CREATE TABLE resultat (
+    id INT PRIMARY KEY
+);
+
+CREATE TABLE pais (
+    id INT PRIMARY KEY
+);
+
+CREATE TABLE piloto_gp (
+    id_piloto INT references piloto(id),
+    id_gp INT references gp(id),
+    PRIMARY KEY (id_piloto, id_gp)
 );
